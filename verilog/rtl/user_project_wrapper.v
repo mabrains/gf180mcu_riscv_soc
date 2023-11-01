86a8c62c998218a229c515aede128eaafecbb633
FaragElsayed2 <farag_agoor@mabrains.com>
2023-11-01 19:43:38 +0200

    Adding final serv with caravel

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -76,11 +76,11 @@ serv_rf_top serv_rf_top(
 	.vss(vss),	// User area ground
 `endif
 
-    .clk(wb_clk_i),
-    .i_rst(wb_rst_i),
-    .i_timer_irq(user_irq[0]),
+    // ================================= Main signals =================================
+    .clk(wb_clk_i),                         // clk        : Clock signal
+    .i_rst(wb_rst_i),                       // i_rst      : Synchronous reset
 
-    // MGMT SoC Wishbone Slave
+    // ================================= Wishbone Slave =================================
     // .wbs_cyc_i(wbs_cyc_i),
     // .wbs_stb_i(wbs_stb_i),
     // .wbs_we_i(wbs_we_i),
@@ -89,18 +89,33 @@ serv_rf_top serv_rf_top(
     // .wbs_dat_i(wbs_dat_i),
     // .wbs_ack_o(wbs_ack_o),
 
-    // .o_dbus_dat(wbs_dat_o), // o_dbus_dat --> OUT --> Data bus write data
+    .o_dbus_dat(wbs_dat_o),                     // o_dbus_dat:  Data bus write data                   === OUT === 32 bit
 
-    // IO Pads
-    // .i_ibus_ack(io_in[0]),
-    // .i_dbus_ack(io_in[1]),
-    // .i_ext_ready(io_in[2]),
+    // ================================= Logic Analyzer =================================
+    .o_ibus_adr(la_data_out[31:0]),             // o_ibus_adr:  Instruction bus address               === OUT === 32 bit
+    .o_dbus_adr(la_data_out[63:32]),            // o_dbus_adr:  Data bus address                      === OUT === 32 bit
 
-    // .o_ibus_cyc(io_out[0]),
-    // .o_dbus_we(io_out[1]),
-    // .o_dbus_cyc(io_out[2]),
-    // .o_mdu_valid(io_out[3])
-    
+    .i_ibus_rdt(la_data_in[31:0]),              // i_ibus_rdt:  Instruction bus read data             === IN  === 32 bit
+    .i_dbus_rdt(la_data_in[63:32]),             // i_dbus_rdt:  Data bus return data                  === IN  === 32 bit
+
+    .i_timer_irq (la_oenb[0]),                  // i_timer_irq: Timer interrupt                       === IN  === 1  bit
+    .i_ibus_ack (la_oenb[1]),                   // i_ibus_ack : Instruction bus cycle ack             === IN  === 1  bit
+    .i_dbus_ack (la_oenb[2]),                   // i_dbus_ack : Data bus return data valid            === IN  === 1  bit
+    .i_ext_ready (la_oenb[3]),                  // i_ext_ready: Extension interface RD contents valid === IN  === 1  bit
+    .i_ext_rd (la_oenb[35:4]),                  // i_ext_rd   : Extension interface RD contents       === IN  === 32 bit
+
+    // ================================= IO Pads =================================
+
+    .o_ibus_cyc(io_out[0]),                     // o_ibus_cyc : Instruction bus active cycle          === OUT === 1  bit
+    .o_dbus_sel(io_out[4:1]),                   // o_dbus_sel : Data bus write data byte select mask  === OUT === 4  bit
+    .o_dbus_we(io_out[5]),                      // o_dbus_we  : Data bus write transaction            === OUT === 1  bit
+    .o_ext_rs1(io_out[37:6]),                   // o_ext_rs1  : Extension interface RS1 contents      === OUT === 32 bit
+
+    .o_ext_rs2(io_oeb[31:0]),                   // o_ext_rs2  : Extension interface RS2 contents      === OUT === 32 bit
+    .o_ext_funct3(io_oeb[34:32]),               // o_ext_func3: Extension interface funct3 contents   === OUT === 3  bit
+    .o_mdu_valid(io_oeb[35]),                   // o_mdu_valid: MDU request                           === OUT === 1  bit
+    .o_dbus_cyc(io_oeb[36])                     // o_dbus_cyc : Data bus active cycle                 === OUT === 1  bit
+
 );
 
 endmodule	// user_project_wrapper
